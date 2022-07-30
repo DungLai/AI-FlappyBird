649cd390e1bc3327ade212e67548f19a414ec4b4
Huy Truong <67343196+huygiatrng@users.noreply.github.com>
2022-07-30 13:40:12 -0400

    adding flap cooldown
    flap cooldown variable at setting

--- Bird.py
+++ Bird.py
@@ -3,34 +3,45 @@ from ANN import *
 
 vec2 = pg.math.Vector2
 
+
 class Bird(pg.sprite.Sprite):
-    def __init__(self, genome = None):
+    def __init__(self, genome=None):
         pg.sprite.Sprite.__init__(self)
-        self.image = bird_img
+        self.ori_img = bird_img
+        self.image = self.ori_img
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, random.randint(15,HEIGHT-SAND_HEIGHT)-5)
+        self.rect.center = (WIDTH / 2, random.randint(15, HEIGHT - SAND_HEIGHT) - 5)
+        self.angle_direction = 0
 
-        #self.pos = vec2(WIDTH/2, HEIGHT/2)
-        self.pos = vec2(WIDTH/2, random.randint(15,HEIGHT-SAND_HEIGHT)-5)
-
-        self.vel = vec2(0,0)
+        # self.pos = vec2(WIDTH/2, HEIGHT/2)
+        self.pos = vec2(WIDTH / 2, random.randint(15, HEIGHT - SAND_HEIGHT) - 5)
+        self.vel = vec2(0, 0)
         self.acc = vec2(0, GRAVITY)
-
-        self.live = 1;
-
+        self.live = 1
         self.sensor = Sensor()
-
+        self.count = 0
         if genome is None:
             self.ANN = ANN()
         else:
             self.ANN = ANN(genome)
-    def update(self):
-        self.vel += self.acc
-        self.pos += self.vel + self.acc
-        self.rect.center = (self.pos[0], self.pos[1])
 
-        if self.live == 1: #if bird alive then increase fitness
+    def update(self):
+        self.count+=1
+        if self.vel[1] > 8:
+            self.vel[1] = 8
+        else:
+            self.vel += self.acc
+
+        self.pos += self.vel + self.acc
+        # self.rect.center = (self.pos[0]-int(self.rotatedImage.get_width()/2), self.pos[1]-int(self.rotatedImage.get_height()/2))
+        # self.rect.center = (self.pos[0], self.pos[1])
+        if self.live == 1:  # if bird alive then increase fitness
+            self.image = pg.transform.rotate(self.ori_img, self.vel.y * (TURN_COEFFICIENT))
+            self.rect.center = (
+            self.pos[0] - int(self.image.get_width() / 2), self.pos[1] - int(self.image.get_height() / 2))
             self.ANN.fitness += FITNESS_RATE
 
     def flap(self):
-        self.vel.y = JUMP_VELOCITY
+        if self.count>FLAP_COOLDOWN:
+            self.vel.y = JUMP_VELOCITY
+        # self.image = pg.transform.rotate(self.ori_img, -30)
